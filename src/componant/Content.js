390e16c4ccf7937fa30f07420880fe8b796b24ce
qusaiqeisi <qusaiqeisi6@gmail.com>
2021-07-04 17:49:46 +0300

    frontend done

---
+++ src/componant/Content.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Content extends Component {
+    render() {
+        return (
+            <div>
+                <h1>this is special content</h1>
+            </div>
+        )
+    }
+}
+
+export default Content
