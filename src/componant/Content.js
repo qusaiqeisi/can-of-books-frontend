d6ba6a88285e4f86b27378a311bff821e88ea671
qusaiqeisi <qusaiqeisi6@gmail.com>
2021-07-04 20:03:07 +0300

    readme

--- src/componant/Content.js
+++ src/componant/Content.js
@@ -1,6 +1,26 @@
+import { withAuth0 } from '@auth0/auth0-react';
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export class Content extends Component {
+    componentDidMount = () => {
+        if(this.props.auth0.isAuthenticated) {
+          this.props.auth0.getIdTokenClaims()
+          .then(res => {
+            const jwt = res.__raw;
+            const config = {
+              headers: {"Authorization" : `Bearer ${jwt}`},
+              method: 'get',
+              baseURL: process.env.REACT_APP_SERVER_URL,
+              url: '/authorize'
+            }
+            axios(config)
+              .then(axiosResults => console.log(axiosResults.data))
+              .catch(err => console.error(err));
+          })
+          .catch(err => console.error(err));
+        }
+      }
     render() {
         return (
             <div>
@@ -10,4 +30,4 @@ export class Content extends Component {
     }
 }
 
-export default Content
+export default withAuth0(Content);
