2394b85d7562b48b2aeb38292d0a3e82b12ca10c
qusaiqeisi <qusaiqeisi6@gmail.com>
2021-07-05 22:43:01 +0300

    lab 12 almost done

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,16 +1,49 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import axios from 'axios';
 import './BestBooks.css';
 
 class MyFavoriteBooks extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      
+      data: ''
+    }
+  }
+  getInput = (e) => {
+    this.setState({
+      data: e.target.value,
+      
+    })
+    console.log(this.state.data)
+  }
+  
+  
+  requestData = (e) => {
+    e.preventDefault()
+    let userEmail = this.state.data
+    
+    let axiosArray= axios.get(`http://localhost:8000/book?email=${userEmail}`).then(res => {
+      console.log(res)
+    }).catch(err => { console.log(err) })
+  }
+  
+  
   render() {
-    return(
+    return (
       <Jumbotron>
-        <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
+        <div>
+          <form>
+            <input type="text" onChange={this.getInput} />
+            <button onClick={(e) => this.requestData(e)}>submit</button>
+          </form>
+          <h2>{this.state.name}</h2>
+          <h2>q</h2>
+          <h2>q</h2>
+        </div>
       </Jumbotron>
     )
   }
