30b9e38cd3b6194fa0cdb6bbbfe0701a37c53ebb
AdhamOmari <adhamomarics@gmail.com>
2021-07-07 17:44:51 +0300

    lab12

--- src/componant/Home.js
+++ src/componant/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 
+
 export class Home extends Component {
     render() {
         return (
             <div>
                 <h1>this is special Home</h1>
+
 
             </div>
         )
